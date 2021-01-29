b00c8db35e59a95b993157be9222dbfc8313c7bf
ddashasa <77571548+ddashasa@users.noreply.github.com>
2021-01-29 21:29:37 +0400

    feat: add functions code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (!(Array.isArray(array)) || array.length == 0) {
+		return 0;
+	};
+
+	let counter = array[0];
+
+  	for (let i = 0; i < array.length-1; i++) {
+  		if (array[i] < counter) {
+  			counter = array[i];
+  		};
+  	};
+
+  	if (counter < array[array.length-1]) {
+  		return counter;
+  	} else {
+  		return array[array.length-1];
+  	}
 }
 
 exports.max = function max (array) {
-  return 0;
+  	if (!(Array.isArray(array)) || array.length == 0) {
+		return 0
+	}
+
+	let counter = array[0];
+
+  	for (let i = 0; i < array.length-1; i++) {
+  		if (array[i] > counter) {
+  			counter = array[i];
+  		};
+  	};
+
+  	if (counter > array[array.length-1]) {
+  		return counter;
+  	} else {
+  		return array[array.length-1];
+  	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (!(Array.isArray(array)) || array.length == 0) {
+		return 0
+	}
+
+	let counter = 0
+
+	for (let i = 0; i < array.length; i++) {
+		counter += array[i];
+	}
+	return counter / array.length
 }
+
